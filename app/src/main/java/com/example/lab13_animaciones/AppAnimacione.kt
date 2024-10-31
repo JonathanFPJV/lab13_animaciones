181d3f78ac2093fd7625fb04fef22a9354325e74
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-10-31 11:50:31 -0500

    implementación de AnimatedContent

--- app/src/main/java/com/example/lab13_animaciones/AppAnimacione.kt
+++ app/src/main/java/com/example/lab13_animaciones/AppAnimacione.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +22,8 @@ fun AppAnimacion() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -31,6 +34,8 @@ fun AppAnimacion() {
             ColorAnimation()  // Aquí se llama directamente a ColorAnimation
             Spacer(modifier = Modifier.height(16.dp))
             SizeAndPositionAnimation()  // Aquí se llama directamente a SizeAndPositionAnimation
+            Spacer(modifier = Modifier.height(16.dp))
+            AnimatedContent()
         }
     }
 }
